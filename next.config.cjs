c2c1fceeeb6471ae20358ade01cb2dfe53742a44
Joel Calado <joelcalado@gmail.com>
2025-09-11 08:48:49 +0100

    feat: update Next.js configuration to set output to 'export'

--- next.config.cjs
+++ next.config.cjs
@@ -3,6 +3,7 @@ const { createNextIntlPlugin } = require("next-intl/plugin");
 const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
 
 const nextConfig = {
+  output: 'export',
   outputFileTracingRoot: __dirname,
   /* config options here */
 };
